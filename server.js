de457673a0e264adc29956962818be3da0c80b9c
Mustafa <apabilim@yahoo.com>
2026-04-28 00:43:22 +0300

    chore: add dotenv support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { google } = require('googleapis');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 80;
